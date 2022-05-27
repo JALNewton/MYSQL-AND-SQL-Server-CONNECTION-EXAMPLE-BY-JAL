3ad29053631bfbc27620e020a0339e3a64bc5220
Jose Araujo <47309489+JALNewton@users.noreply.github.com>
2022-05-27 10:26:39 -0300

    Update SQLDB.cs
    Att private para public

--- SQLDB.cs
+++ SQLDB.cs
@@ -105,7 +105,7 @@ namespace SQLDB_byJAL
 
         
 
-        private bool Close()
+        public bool Close()
 
         {
 
@@ -119,7 +119,7 @@ namespace SQLDB_byJAL
 
             }
 
-            catch (MySqlException ex)
+            catch (SqlException ex)
 
             {
 
